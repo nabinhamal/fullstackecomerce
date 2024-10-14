ea10f182b009d29d68c76dea574c44fc2256502d
nabin <nabinhamal2@gmail.com>
2024-10-14 17:17:39 +0545

    order and user schema added

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -6,13 +6,13 @@ import {
   updateProduct,
   deleteProduct,
 } from "./productsController";
-// import { validateData } from "../../middlewares/validationMiddleware";
+import { validateData } from "../../middlewares/validationMiddleware";
 
 import {
   createProductSchema,
   updateProductSchema,
 } from "../../db/productsSchema";
-// import { verifySeller, verifyToken } from "../../middlewares/authMiddleware";
+import { verifySeller, verifyToken } from "../../middlewares/authMiddleware";
 
 const router = Router();
 
@@ -21,13 +21,15 @@ router.get("/:id", getProductById);
 router.post(
   "/",
 
+  validateData(createProductSchema),
   createProduct
 );
 router.put(
   "/:id",
 
+  validateData(updateProductSchema),
   updateProduct
 );
-router.delete("/:id", deleteProduct);
+router.delete("/:id", verifyToken, verifySeller, deleteProduct);
 
 export default router;
