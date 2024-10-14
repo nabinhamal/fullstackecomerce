3c5181ca425a4a0ba37cc715f47874ea40ed09c8
nabin <nabinhamal2@gmail.com>
2024-10-14 16:23:01 +0545

    new project

---
+++ api/src/index.ts
@@ -0,0 +1,20 @@
+import express, { json, urlencoded } from "express";
+
+const port = 3000;
+const app = express();
+
+app.use(urlencoded({ extended: false }));
+app.use(json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// app.use("/products", productsRoutes);
+// app.use("/auth", authRoutes);
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
+
+// export const handler = serverless(app);
