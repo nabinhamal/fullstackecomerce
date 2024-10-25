e66b8a92efe86fa31b7ebadd2b17ba786e0e5914
nabin <nabinhamal2@gmail.com>
2024-10-25 10:08:14 +0545

    api added for auth orders and products

--- ecom-mobile/app/_layout.tsx
+++ ecom-mobile/app/_layout.tsx
@@ -1,14 +1,19 @@
 import "@/global.css";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { Stack } from "expo-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
-    <GluestackUIProvider>
-      <Stack>
-        <Stack.Screen name="index" options={{ title: "shop" }} />
-        <Stack.Screen name="product/[id]" options={{ title: "Product" }} />
-      </Stack>
-    </GluestackUIProvider>
+    <QueryClientProvider client={queryClient}>
+      <GluestackUIProvider>
+        <Stack>
+          <Stack.Screen name="index" options={{ title: "shop" }} />
+          <Stack.Screen name="product/[id]" options={{ title: "Product" }} />
+        </Stack>
+      </GluestackUIProvider>
+    </QueryClientProvider>
   );
 }
--- ecom-mobile/app/index.tsx
+++ ecom-mobile/app/index.tsx
@@ -1,21 +1,32 @@
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  useWindowDimensions,
-  View,
-} from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import products from "../assets/product.json";
-import { FlatList } from "react-native";
+import { ActivityIndicator, FlatList } from "react-native";
 import ProductListItem from "../components/ProductListItem";
 import { Button, ButtonText } from "@/components/ui/button";
 import { useBreakpointValue } from "@/components/ui/utils/use-break-point-value";
+import { listProducts } from "@/api/products";
+import { useQuery } from "@tanstack/react-query";
+import { Text } from "@/components/ui/text";
 
 export default function HomeScreen() {
-  const { width } = useWindowDimensions();
+  // const { width } = useWindowDimensions();
   // const numColumns = width > 700 ? 3 : 2;
+  // const [products, setProducts] = useState();
+
+  // useEffect(() => {
+  //   const fetchProducts = async () => {
+  //     const data = await listProducts();
+  //     setProducts(data);
+  //   };
+
+  //   fetchProducts();
+  // }, []);
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["products"],
+    queryFn: listProducts,
+  });
 
   const numColumns = useBreakpointValue({
     default: 2,
@@ -23,10 +34,18 @@ export default function HomeScreen() {
     xl: 4,
     xs: 1,
   });
+
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+
+  if (error) {
+    return <Text>Error fetching products</Text>;
+  }
   return (
     <FlatList
       key={numColumns}
-      data={products}
+      data={data}
       numColumns={numColumns}
       contentContainerClassName="gap-2 max-w-[960px] mx-auto w-full"
       columnWrapperClassName="gap-2"
