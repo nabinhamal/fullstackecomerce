448704ba071af3751ad08caea639ae75fd858979
nabin <nabinhamal2@gmail.com>
2024-10-28 10:30:13 +0545

    new updated

--- dashboard/api/products.ts
+++ dashboard/api/products.ts
@@ -3,7 +3,6 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL;
 export async function listProducts() {
   const res = await fetch(`${API_URL}/products`);
   const data = await res.json();
-  console.log(data);
   if (!res.ok) {
     throw new Error("Error");
   }
